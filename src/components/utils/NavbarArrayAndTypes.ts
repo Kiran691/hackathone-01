74a71e73a099fce787c01b9de686be2d150257dc
Kiran <kiranimran691@gmail.com>
2023-07-24 19:37:27 +0500

    first commit

---
+++ src/components/utils/NavbarArrayAndTypes.ts
@@ -0,0 +1,73 @@
+export interface NavbarItemType{
+    label: string,
+    href: string,
+    isDropDown: boolean,
+    dropDownData?: Array<NavbarItemType>,
+
+};
+
+
+
+
+export const NavbarArray: Array<NavbarItemType> =[
+    {
+        label: "Female",
+        href: "/female ",
+        isDropDown: true,
+        dropDownData:[
+            {
+                label: "Dresses",
+                href: "/female/dresse",
+                isDropDown: false,
+            },
+            {
+                label: "T-Shirts",
+                href: "/female/t-shirt",
+                isDropDown: false,
+            },
+            {
+                label: "Pents",
+                href: "/female/pent",
+                isDropDown: false,
+            },
+            {
+                label: "Jackets",
+                href: "/female/jacket",
+                isDropDown: false,
+            },
+            {
+                label: "Sweater",
+                href: "/female/sweater",
+                isDropDown: false,
+            }
+        ]
+    },
+    {
+        label: "Male",
+        href: "/male ",
+        isDropDown: true,
+        dropDownData:[
+            {
+                label: "Sweaters",
+                href: "/male/sweater",
+                isDropDown: false,
+            },
+            
+            {
+                label: "Jackets",
+                href: "/male/jacket",
+                isDropDown: false,
+            }
+        ]
+    },
+    {
+        label: "Kids",
+        href: "/kid ",
+        isDropDown: false,
+    },
+    {
+        label: "All Products",
+        href: "/allproduct ",
+        isDropDown: false,
+    },
+]

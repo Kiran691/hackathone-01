cf059182fec9edfb0c4e09db424133f82ad9cf6f
Kiran <kiranimran691@gmail.com>
2023-07-24 19:38:20 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,19 @@
-import BASE_PATH_FORAPI from "@/components/shared/Wrapper/BasePath";
+//import BASE_PATH_FORAPI from "@/components/shared/Wrapper/BasePath";
 import Hero from "@/components/views/Hero";
 import ProductsType from "@/components/views/ProductsType";
 
-async function fetchAIIProductsData() {
-  let res = await fetch(`${BASE_PATH_FORAPI}/api/products`);
+// async function fetchAIIProductsData() {
+//   let res = await fetch(`${BASE_PATH_FORAPI}/api/products`);
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch");
-  }
-  return res.json();
-}
+//   if (!res.ok) {
+//     throw new Error("Failed to fetch");
+//   }
+//   return res.json();
+// }
 
 export default async function Home() {
-  let response = await fetchAIIProductsData();
-  console.log("response :", response);
+  //   let response = await fetchAIIProductsData();
+  //   console.log("response :", response);
 
   return (
     <div>

44c987967b2203616d4dd1743c2ec0cd2978bc31
Kiran <kiranimran691@gmail.com>
2023-07-26 11:16:41 +0500

    solved some error of deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,25 @@
-//import BASE_PATH_FORAPI from "@/components/shared/Wrapper/BasePath";
+import BASE_PATH_FORAPI from "@/components/shared/Wrapper/BasePath";
 import Hero from "@/components/views/Hero";
 import ProductsType from "@/components/views/ProductsType";
+import ProductCarousel from "@/components/views/ProductCarousel";
 
-// async function fetchAIIProductsData() {
-//   let res = await fetch(`${BASE_PATH_FORAPI}/api/products`);
-
-//   if (!res.ok) {
-//     throw new Error("Failed to fetch");
-//   }
-//   return res.json();
-// }
+async function fetchAIIProductsData() {
+  // let res = await fetch(`${BASE_PATH_FORAPI}/api/products`);
+  // if (!res.ok) {
+  //   throw new Error("Failed to fetch");
+  // }
+  // return res.json();
+  return { response: "Hello" };
+}
 
 export default async function Home() {
-  //   let response = await fetchAIIProductsData();
-  //   console.log("response :", response);
+  let response = await fetchAIIProductsData();
 
   return (
     <div>
-      {/* <Hero /> */}
-      {/* <ProductsType /> */}
+      <Hero />
+      <ProductsType />
+      <ProductCarousel ProductData={response} />
     </div>
   );
 }
--- src/components/shared/Wrapper/BasePath.ts
+++ src/components/shared/Wrapper/BasePath.ts
@@ -1,5 +1,5 @@
-// const BASE_PATH_FORAPI = process.env.NODE_ENV === "development"?
-// "http://localhost:3000"
-// :""
+const BASE_PATH_FORAPI = process.env.NODE_ENV === "development"?
+"http://localhost:3000"
+:"https//hackathone-1.vercel.app"
 
-// export default BASE_PATH_FORAPI
+export default BASE_PATH_FORAPI
